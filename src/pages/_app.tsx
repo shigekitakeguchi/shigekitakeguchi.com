9d7974ce083271a61d520dedb433a84f57cb9874
Shigeki Takeguchi <shigeki.takeguchi@E790559.local>
2025-05-12 10:52:09 +0900

    いったんClineで開発するために空にする

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppProps } from "next/app";
-import "@/styles/normalize.css";
-import "@/styles/globals.scss";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
